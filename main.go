e668824da046d1b293e61d770edde7c63f65da8b
Aleksandrs Stukalovs <astukalovs@edu.riga.lv>
2023-01-26 10:34:52 +0200

    Yaroslav's corrections

--- main.go
+++ main.go
@@ -34,37 +34,59 @@ func main() {
 var d float64
 
 func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
+
 	img.Set(x1, y1, color.RGBA{1, 255, 1, 255})
 	img.Set(x2, y2, color.RGBA{1, 255, 1, 255})
 
-	Dx, Dy := x2-x1, y2-y1
-	k := float64(Dy) / float64(Dx)
-	if math.Abs(k) <= 1 {
+	// abs(Dy) < abs(dx) | / abs(dx) => abs(Dy)/abs(Dx) < 1 == abs(k) < 1
+	if math.Abs(float64(y2-y1)) < math.Abs(float64(x2-x1)) {
+		if x1 > x2 {
+			x1, x2 = x2, x1
+			y1, y2 = y2, y1
+		}
+		Dx, Dy := x2-x1, y2-y1
+		dirY := 1
+		midY := 0.5
+		if Dy < 0 {
+			dirY = -1
+			midY = -0.5
+		}
+		f := func(x, y float64) float64 {
+			A, B, C := Dy, -Dx, Dx*y1-Dy*x1
+			return float64(A)*x + float64(B)*y + float64(C)
+		}
 		for x, y := x1, y1; x < x2; x++ {
 			img.Set(x, y, c)
-			xm, ym := float64(x+1), float64(y)-0.5
-			f := func(x, y float64) float64 {
-				A, B, C := Dy, -Dx, Dx*y1-Dy*x1
-				return float64(A)*x + float64(B)*y + float64(C)
-			}
-			d = f(xm, ym)
-			if d < 0 {
-				y--
+			xm, ym := float64(x)+1, float64(y)+midY
+			d = f(xm, ym) * float64(dirY)
+			if d > 0 {
+				y += dirY
 			}
 		}
 	} else {
-		for x, y := x1, y1; y > y2; y-- {
+		if y1 > y2 {
+			x1, x2 = x2, x1
+			y1, y2 = y2, y1
+		}
+		Dx, Dy := x2-x1, y2-y1
+		dirX := 1
+		midX := 0.5
+		if Dx < 0 {
+			dirX = -1
+			midX = -0.5
+		}
+		f := func(x, y float64) float64 {
+			A, B, C := Dy, -Dx, Dx*y1-Dy*x1
+			return float64(A)*x + float64(B)*y + float64(C)
+		}
+		for x, y := x1, y1; y < y2; y++ {
 			img.Set(x, y, c)
-			xm, ym := float64(x)+0.5, float64(y-1)
-			f := func(x, y float64) float64 {
-				A, B, C := -Dy, Dx, Dx*x1-Dy*y1
-				return float64(A)*y + float64(B)*x + float64(C)
-			}
-			d = f(xm, ym)
-			if d < 0 {
-				x++
+			xm, ym := float64(x)+midX, float64(y)+1
+			d = f(ym, xm) * float64(dirX)
+			if d > 0 {
+				x += dirX
 			}
 		}
-	}
 
+	}
 }
