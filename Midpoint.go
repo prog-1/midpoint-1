2393644e10066c85442e911e847f736edfcd9136
vay4ie <75880193+vay4ie@users.noreply.github.com>
2023-01-26 08:39:26 +0200

    Added y sign change

--- Midpoint.go
+++ Midpoint.go
@@ -30,29 +30,38 @@ func (g *Game) Update() error {
 //---------------------------Draw-------------------------------------
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	g.DrawLine(screen, screenWidth/2, screenHeight/2, screenWidth/2+200, screenHeight/2+100, color.RGBA{255, 255, 255, 255})
+	g.DrawLine(screen, screenWidth/2, screenHeight/2, screenWidth/2+100, screenHeight/2-100, color.RGBA{255, 255, 255, 255})
 }
 
 //-------------------------Functions----------------------------------
 
 func (g *Game) DrawLine(screen *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 
-	A := y2 - y1 //Δy
-	B := x1 - x2 // -Δx
+	//For debug
+	screen.Set(x1, y1, c) //starting point
+	screen.Set(x2, y2, c) //ending point
 
+	s := 1 //sign of y (+ or -)
+	if y2 < y1 {
+		s = -1
+	}
+
+	A := y2 - y1      //Δy
+	B := x1 - x2      // -Δx
 	C := -B*y1 - A*x1 // C = Δx * y1 - Δy *x1
 
 	fl := func(v int) float64 { return float64(v) } //to make formula shorter
 
 	for x, y := x1, y1; x < x2; x++ {
 
-		f := fl(A)*fl(x) + fl(B)*(fl(y)-0.5) + fl(C) //Ax + By + C
+		f := fl(A)*fl(x) + fl(B)*(fl(y)+(0.5*fl(s))) + fl(C) //Ax + By + C
+		//B*y-0.5 to up, B*y+0.5 to down
 
 		//if f < 0 fill the pixel on (x+1; y)
 		if f >= 0 { //fill the pixel on (x+1; y+1)
-			y++
+			y = y + s //y-- to up, y++ to down
 		}
-		screen.Set(x, y, c)
+		screen.Set(x, y, c) //filling the pixel
 
 	}
 }
