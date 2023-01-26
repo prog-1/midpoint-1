49c4fc1b148d91a8e88e5699feb51d29385243bd
Andrej Ceremnih <andrejceremnih@gmail.com>
2023-01-26 08:54:21 +0200

    still not done

--- main.go
+++ main.go
@@ -37,34 +37,41 @@ func NewLine(x1, y1, x2, y2 int) *Line {
 }
 
 func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
-	if x2 < x1 {
-		x1, x2 = x2, x1
-		y1, y2 = y2, y1
-	}
-	x, y := x1, y1
 	A := (float64(y2 - y1))
 	B := -(float64(x2 - x1))
 	C := -B*float64(y1) - A*float64(x1)
 	if A <= -B {
-		dy := 1
-		if y1 > y2 {
-			dy = -1
+		dirY := 1
+		if A < 0 { // dy < 0
+			dirY = -1
 		}
+		if -B < 0 { // dx < 0
+			x1, y1, x2, y2 = x2, y2, x1, y1
+		}
+		x, y := x1, y1
 		for x <= x2 {
 			img.Set(x, y, c)
 			f := A*float64(x+1) + B*(float64(y)+0.5) + C
 			x += 1
-			if f > 0 {
-				y += dy
+			if float64(dirY)*f > 0 {
+				y += dirY
 			}
 		}
 	} else { // A > -B
+		dirX := 1
+		if -B < 0 { // dx < 0
+			dirX = -1
+		}
+		if A < 0 { // dy < 0
+			x1, y1, x2, y2 = x2, y2, x1, y1
+		}
+		x, y := x1, y1
 		for y <= y2 {
 			img.Set(x, y, c)
 			f := A*(float64(x)+0.5) + B*float64(y+1) + C
 			y += 1
-			if f > 0 {
-				x += 1
+			if float64(dirX)*f > 0 {
+				x += dirX
 			}
 		}
 	}
@@ -74,7 +81,7 @@ func (l *Line) Draw(screen *ebiten.Image) {
 	DrawLine(screen, l.pos1.x, l.pos1.y, l.pos2.x, l.pos2.y, l.color)
 	DrawLine(screen, 300, 100, 100, 420, l.color) // additional
 	DrawLine(screen, 500, 100, 500, 450, l.color) // additional  ?
-	DrawLine(screen, 50, 450, 500, 450, l.color)  // additional  ?
+	DrawLine(screen, 50, 450, 500, 450, l.color)  // additional
 	DrawLine(screen, 80, 300, 110, 50, l.color)   // additional  ?
 }
 
