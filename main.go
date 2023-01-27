f83186c96ce7aa93c62f701a7ddacbe6466b7ddf
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-01-27 04:42:26 +0200

    Fixed mid point algorithm

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
 	"log"
 	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 const (
@@ -29,9 +27,6 @@ type game struct {
 func (g *game) Layout(outWidth, outHeight int) (w, h int) { return screenWidth, screenHeight }
 func (g *game) Update() error {
 	g.l.radians += math.Pi / 180
-	if g.l.radians > math.Pi/2 {
-		g.l.radians = 0
-	}
 	return nil
 }
 func (g *game) Draw(screen *ebiten.Image) {
@@ -39,7 +34,6 @@ func (g *game) Draw(screen *ebiten.Image) {
 	y := float64(g.l.length) * math.Sin(g.l.radians)
 	x2, y2 := g.l.x1+int(x), g.l.y1+int(y)
 	DrawLine(screen, g.l.x1, g.l.y1, x2, y2, g.l.c)
-	ebitenutil.DebugPrint(screen, fmt.Sprint(d))
 }
 
 func main() {
@@ -50,13 +44,7 @@ func main() {
 	}
 }
 
-var d float64
-
 func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
-
-	img.Set(x1, y1, color.RGBA{1, 255, 1, 255})
-	img.Set(x2, y2, color.RGBA{1, 255, 1, 255})
-
 	// abs(Dy) < abs(dx) | / abs(dx) => abs(Dy)/abs(Dx) < 1 == abs(k) < 1
 	if math.Abs(float64(y2-y1)) < math.Abs(float64(x2-x1)) {
 		if x1 > x2 {
@@ -66,6 +54,7 @@ func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 		Dx, Dy := x2-x1, y2-y1
 		dirY := 1
 		midY := 0.5
+		// Dy < 0 => y1 > y2 => Drawing downwards
 		if Dy < 0 {
 			dirY = -1
 			midY = -0.5
@@ -77,7 +66,7 @@ func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 		for x, y := x1, y1; x < x2; x++ {
 			img.Set(x, y, c)
 			xm, ym := float64(x)+1, float64(y)+midY
-			d = f(xm, ym) * float64(dirY)
+			d := f(xm, ym) * float64(dirY)
 			if d > 0 {
 				y += dirY
 			}
@@ -90,22 +79,25 @@ func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 		Dx, Dy := x2-x1, y2-y1
 		dirX := 1
 		midX := 0.5
+		// Dx < 0 => x1 > x2 => Drawing downwards
 		if Dx < 0 {
 			dirX = -1
 			midX = -0.5
 		}
 		f := func(x, y float64) float64 {
-			A, B, C := Dy, -Dx, Dx*y1-Dy*x1
+			A, B, C := Dx, -Dy, Dy*x1-Dx*y1
 			return float64(A)*x + float64(B)*y + float64(C)
 		}
 		for x, y := x1, y1; y < y2; y++ {
 			img.Set(x, y, c)
 			xm, ym := float64(x)+midX, float64(y)+1
-			d = f(ym, xm) * float64(dirX)
+			d := f(ym, xm) * float64(dirX)
 			if d > 0 {
 				x += dirX
 			}
 		}
-
 	}
+
+	img.Set(x1, y1, color.RGBA{1, 255, 1, 255})
+	img.Set(x2, y2, color.RGBA{1, 255, 1, 255})
 }
