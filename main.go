a3449a92acf547d20acef87938097636fa21293f
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-01-27 07:29:22 +0200

    Implemented unoptimized (Bresenham's?) algorithm

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
 	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 const (
@@ -34,6 +36,7 @@ func (g *game) Draw(screen *ebiten.Image) {
 	y := float64(g.l.length) * math.Sin(g.l.radians)
 	x2, y2 := g.l.x1+int(x), g.l.y1+int(y)
 	DrawLine(screen, g.l.x1, g.l.y1, x2, y2, g.l.c)
+	ebitenutil.DebugPrint(screen, fmt.Sprint(k))
 }
 
 func main() {
@@ -44,6 +47,8 @@ func main() {
 	}
 }
 
+var k float64
+
 func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 	// abs(Dy) < abs(dx) | / abs(dx) => abs(Dy)/abs(Dx) < 1 == abs(k) < 1
 	if math.Abs(float64(y2-y1)) < math.Abs(float64(x2-x1)) {
@@ -51,50 +56,20 @@ func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 			x1, x2 = x2, x1
 			y1, y2 = y2, y1
 		}
-		Dx, Dy := x2-x1, y2-y1
-		dirY := 1
-		midY := 0.5
-		// Dy < 0 => y1 > y2 => Drawing downwards
-		if Dy < 0 {
-			dirY = -1
-			midY = -0.5
-		}
-		f := func(x, y float64) float64 {
-			A, B, C := Dy, -Dx, Dx*y1-Dy*x1
-			return float64(A)*x + float64(B)*y + float64(C)
-		}
-		for x, y := x1, y1; x < x2; x++ {
-			img.Set(x, y, c)
-			xm, ym := float64(x)+1, float64(y)+midY
-			d := f(xm, ym) * float64(dirY)
-			if d > 0 {
-				y += dirY
-			}
+		k = float64(y2-y1) / float64(x2-x1)
+		for x, y := x1, float64(y1); x < x2; x++ {
+			img.Set(x, int(math.Round(y)), c)
+			y += k
 		}
 	} else {
 		if y1 > y2 {
 			x1, x2 = x2, x1
 			y1, y2 = y2, y1
 		}
-		Dx, Dy := x2-x1, y2-y1
-		dirX := 1
-		midX := 0.5
-		// Dx < 0 => x1 > x2 => Drawing downwards
-		if Dx < 0 {
-			dirX = -1
-			midX = -0.5
-		}
-		f := func(x, y float64) float64 {
-			A, B, C := Dx, -Dy, Dy*x1-Dx*y1
-			return float64(A)*x + float64(B)*y + float64(C)
-		}
-		for x, y := x1, y1; y < y2; y++ {
-			img.Set(x, y, c)
-			xm, ym := float64(x)+midX, float64(y)+1
-			d := f(ym, xm) * float64(dirX)
-			if d > 0 {
-				x += dirX
-			}
+		k = float64(x2-x1) / float64(y2-y1)
+		for x, y := float64(x1), y1; y < y2; y++ {
+			img.Set(int(x), y, c)
+			x += k
 		}
 	}
 
