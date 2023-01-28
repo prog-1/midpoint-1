bb8d12ec38ab05754a72506f688e9bbcc361d31f
vay4ie <75880193+vay4ie@users.noreply.github.com>
2023-01-28 16:11:43 +0200

    Implemented line rotation

--- Midpoint.go
+++ Midpoint.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -15,22 +16,43 @@ const (
 )
 
 type Game struct {
-	width, height int
 	//here all the global variables are stored
+	width, height int   //screen size
+	l             *line //line struct
+}
+
+type line struct {
+	x1, y1    int     //starting point
+	x2, y2    int     //ending point
+	magnitude float64 //line length
+	radians   float64 //ending point angle
 }
 
 //---------------------------Update-------------------------------------
 
 func (g *Game) Update() error {
 	//all logic on update
-	//(can be divided on seperate functions for ex: "UpdateCircle")
+
+	//increasing rotation
+	g.l.radians += math.Pi / 180
+
 	return nil
 }
 
 //---------------------------Draw-------------------------------------
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	g.DrawLine(screen, screenWidth/2, screenHeight/2, screenWidth/2+100, screenHeight/2-100, color.RGBA{255, 255, 255, 255})
+
+	//Ending point calculations with some fancy formulas
+	x := int(math.Cos(g.l.radians) * g.l.magnitude)
+	y := int(math.Sin(g.l.radians) * g.l.magnitude)
+
+	//adding starting point to values
+	g.l.x2 = g.l.x1 + x
+	g.l.y2 = g.l.y1 + y
+
+	//Line Draw
+	g.DrawLine(screen, g.l.x1, g.l.y1, g.l.x2, g.l.y2, color.RGBA{255, 255, 255, 255}) //x2 = screenWidth/2+100 | y2 = screenHeight/2-100
 }
 
 //-------------------------Functions----------------------------------
@@ -75,14 +97,14 @@ func (g *Game) Layout(inWidth, inHeight int) (outWidth, outHeight int) {
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Midpoint")
-	ebiten.SetWindowResizable(true)           //enablening window resize
-	g := NewGame(screenWidth, screenHeight)   //creating game instance
-	if err := ebiten.RunGame(g); err != nil { //running game
+	ebiten.SetWindowResizable(true) //enablening window resize
+
+	//creating game instance
+	g := &Game{width: screenWidth, height: screenHeight,
+		l: &line{x1: screenWidth / 2, y1: screenHeight / 2, x2: 100, y2: 0, magnitude: 100, radians: 0}} //configuring line
+
+	//running game
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
-}
-
-//New game instance creation
-func NewGame(width, height int) *Game {
-	return &Game{width: width, height: height}
 }
