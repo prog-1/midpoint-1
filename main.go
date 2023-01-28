3a48c83667781252c1646014b346c9ef674860fc
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-01-28 06:46:36 +0200

    Implemented Bresenham's on int's

--- main.go
+++ main.go
@@ -27,9 +27,6 @@ type game struct {
 func (g *game) Layout(outWidth, outHeight int) (w, h int) { return screenWidth, screenHeight }
 func (g *game) Update() error {
 	g.l.radians += math.Pi / 180
-	// if g.l.radians < -math.Pi/4 {
-	// 	g.l.radians = 0
-	// }
 	return nil
 }
 func (g *game) Draw(screen *ebiten.Image) {
@@ -54,14 +51,14 @@ func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 			x1, x2 = x2, x1
 			y1, y2 = y2, y1
 		}
-		dx, dy := float64(x2-x1), float64(y2-y1)
+		dx, dy := x2-x1, y2-y1
 		dirY := 1
 		// Dy < 0 => y2 - y1 < 0 => y1 > y2 => Growing downwards
 		if dy < 0 {
 			dirY = -1
 			dy = -dy // For us to pretend that line is growing upwards
 		}
-		d := dy - dx/2
+		d := 2*dy - dx
 		for x, y := x1, y1; x < x2; x++ {
 			img.Set(x, y, c)
 			if d >= 0 { // NE
@@ -76,14 +73,13 @@ func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 			x1, x2 = x2, x1
 			y1, y2 = y2, y1
 		}
-		dx, dy := float64(x2-x1), float64(y2-y1)
+		dx, dy := x2-x1, y2-y1
 		dirX := 1
-
 		if dx < 0 {
 			dirX = -1
 			dx = -dx
 		}
-		d := dx - dy/2
+		d := 2*dx - dy
 		for x, y := x1, y1; y < y2; y++ {
 			img.Set(x, y, c)
 			if d >= 0 { // NE
@@ -98,9 +94,3 @@ func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 	img.Set(x1, y1, color.RGBA{1, 255, 1, 255})
 	img.Set(x2, y2, color.RGBA{1, 255, 1, 255})
 }
-
-// Algorithm:
-// General: If Dn > 0 => NE and Dn+1 = Dne, else E and Dn+1 = De
-// 1) Calculate D0 - distance from the first middle point
-//    to the point of the line, which has the same x coordinate.
-// 2) Calculate
