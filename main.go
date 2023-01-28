d592f42600744b374607a525b3e49562db9ca654
Aleksandr Stukalov <stukalovsaleksandrs@gmail.com>
2023-01-28 06:35:52 +0200

    Implemented Bresenham's on floats for k < 1

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"image/color"
 	"log"
 	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 const (
@@ -29,6 +27,9 @@ type game struct {
 func (g *game) Layout(outWidth, outHeight int) (w, h int) { return screenWidth, screenHeight }
 func (g *game) Update() error {
 	g.l.radians += math.Pi / 180
+	// if g.l.radians < -math.Pi/4 {
+	// 	g.l.radians = 0
+	// }
 	return nil
 }
 func (g *game) Draw(screen *ebiten.Image) {
@@ -36,7 +37,6 @@ func (g *game) Draw(screen *ebiten.Image) {
 	y := float64(g.l.length) * math.Sin(g.l.radians)
 	x2, y2 := g.l.x1+int(x), g.l.y1+int(y)
 	DrawLine(screen, g.l.x1, g.l.y1, x2, y2, g.l.c)
-	ebitenutil.DebugPrint(screen, fmt.Sprint(k))
 }
 
 func main() {
@@ -47,8 +47,6 @@ func main() {
 	}
 }
 
-var k float64
-
 func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 	// abs(Dy) < abs(dx) | / abs(dx) => abs(Dy)/abs(Dx) < 1 == abs(k) < 1
 	if math.Abs(float64(y2-y1)) < math.Abs(float64(x2-x1)) {
@@ -56,23 +54,31 @@ func DrawLine(img *ebiten.Image, x1, y1, x2, y2 int, c color.Color) {
 			x1, x2 = x2, x1
 			y1, y2 = y2, y1
 		}
-		k = float64(y2-y1) / float64(x2-x1)
-		for x, y := x1, float64(y1); x < x2; x++ {
-			img.Set(x, int(math.Round(y)), c)
-			y += k
+		dx, dy := float64(x2-x1), float64(y2-y1)
+		dirY := 1
+		// Dy < 0 => y2 - y1 < 0 => y1 > y2 => Growing downwards
+		if dy < 0 {
+			dirY = -1
+			dy = -dy // For us to pretend that line is growing upwards
 		}
-	} else {
-		if y1 > y2 {
-			x1, x2 = x2, x1
-			y1, y2 = y2, y1
-		}
-		k = float64(x2-x1) / float64(y2-y1)
-		for x, y := float64(x1), y1; y < y2; y++ {
-			img.Set(int(x), y, c)
-			x += k
+		d := dy - dx/2
+		for x, y := x1, y1; x < x2; x++ {
+			img.Set(x, y, c)
+			if d >= 0 { // NE
+				y += dirY
+				d += dy - dx
+			} else { // E
+				d += dy
+			}
 		}
 	}
 
 	img.Set(x1, y1, color.RGBA{1, 255, 1, 255})
 	img.Set(x2, y2, color.RGBA{1, 255, 1, 255})
 }
+
+// Algorithm:
+// General: If Dn > 0 => NE and Dn+1 = Dne, else E and Dn+1 = De
+// 1) Calculate D0 - distance from the first middle point
+//    to the point of the line, which has the same x coordinate.
+// 2) Calculate
